86afdd6f4ec6806778107141b46d4ba9c7a9098a
Bogdan-K-A <kozhoma_b@ukr.net>
2021-10-16 14:04:08 +0300

    pet-project

---
+++ js/dropMenu.js
@@ -0,0 +1,28 @@
+// const dropMenu = document.querySelector('.nav-link');
+// console.log(dropMenu);
+
+// dropMenu.addEventListener('mouseover', showMenu);
+// dropMenu.addEventListener('mouseout', hideMenu);
+// // dropMenu.addEventListener('mouseover', hideMenu);
+
+// // dropMenu.forEach(el => {
+// //   el.el.addEventListener('mouseout', () => {
+// //     console.log('убрал');
+// //     if (!e.target.parentNode.lastElementChild === focus) {
+// //       e.target.parentNode.lastElementChild.classList.add('show-submenu');
+// //       //   e.target.parentNode.lastElementChild.classList.add('show-submenu');
+// //     }
+// //   });
+// // });
+
+// function showMenu(e) {
+//   console.log('навёл');
+//   const dropEl = e.target.parentNode.lastElementChild;
+
+//   dropEl.classList.remove('nav-link-img:hover');
+// }
+// function hideMenu(e) {
+//   console.log('убрал');
+//   const dropEl = e.target.parentNode.lastElementChild;
+//   dropEl.classList.add('hiden-submenu');
+// }
